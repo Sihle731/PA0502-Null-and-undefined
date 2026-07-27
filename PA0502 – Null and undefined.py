d9d515fa6a94c992181947b698a6f9f9135a5d4f
Andile Andre3000 <lasmkhwanazi@gmail.com>
2026-07-27 10:35:57 +0100

    Add files via upload

---
+++ PA0502 – Null and undefined.py
@@ -0,0 +1,17 @@
+'''Declare a variable called email
+Do not assign a value
+Print it'''
+
+print(email)
+
+
+'''Declare another variable called phoneNumber
+Assign it the value null
+Print it
+Print the type of both variables'''
+
+phoneNumber = None
+print(phoneNumber)
+
+print(type(email))
+print(type(phoneNumber))
